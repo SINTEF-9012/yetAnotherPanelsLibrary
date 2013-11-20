1aa692ff0c05d2ee140a534325a4e85ac4153e24
Antoine Pultier <antoine.pultier@sintef.no>
2013-11-20 17:29:32 +0100

    Sample build

---
+++ tests/script.js
@@ -0,0 +1,13 @@
+if (navigator.userAgent.match(/iPad;.*CPU.*OS 7_\d/i)) {
+    document.body.parentElement.className = "ipad ios7";
+
+    window.addEventListener('orientationchange', function () {
+        window.scrollTo(0, 0);
+    });
+}
+
+var mainPanel = $('#main'), leftPanel = $('#left'), rightPanel = $('#right'), topPanel = $('#top'), bottomPanel = $('#bottom'), jwindow = $(window);
+
+var layout = new yetAnotherPanelsLibrary(mainPanel, true, false);
+
+layout.setTopPanel(topPanel, true).setLeftPanel(leftPanel, false).setRightPanel(rightPanel, false).setBottomPanel(bottomPanel, false).updateView();
